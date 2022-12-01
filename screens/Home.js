da4cc97cf731fd9b6c83318d52852419212b4c12
Inhoob <inhoob@gmail.com>
2022-12-02 01:34:47 +0900

    Level1 마무리

---
+++ screens/Home.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+function Home() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+export default Home;
---
+++ screens/Library.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+function Library() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Library</Text>
+    </View>
+  );
+}
+export default Library;
