425bc263ac802810cdd063e05894c69771d41492
Inhoob <inhoob@gmail.com>
2022-12-01 22:49:57 +0900

    프로젝트 생성

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import BottomNavigation from "./components/BottomNavigation";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={S.appContainer}>
+      <BottomNavigation />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const S = StyleSheet.create({
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
